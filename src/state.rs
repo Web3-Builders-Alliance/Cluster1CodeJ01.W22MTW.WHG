69ed04362d39c72d981a0837bed4168a34ba3de9
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-11-13 16:15:39 +0000

    code journal

--- src/state.rs
+++ src/state.rs
@@ -4,29 +4,31 @@ use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::{Addr, Coin};
 use cw_storage_plus::{Item, Map};
-
+//make the Config struct public
+//we should make public too
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct Config {  //make the Config struct public 
-    pub owner: Addr  //we should make public too
+pub struct Config {   
+    pub owner: Addr  
 }
 
-
+// I would change this to u128
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct Deposits {
-    pub count: i32, // I would change this to u128
+    pub count: i32, 
     pub owner: Addr,
     pub coins: Coin
 }
-
+//signed integers alovede negative value, smart contract use unsigned (u128)
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct Withdraws {
-    pub count: u128, //signed integers alovede negative value, smart contract use unsigned (u128)
+    pub count: u128, 
     pub owner: Addr,
     pub coins: Coin
 }
 
 //key is address, denom
-pub const DEPOSITS: Map<(&str, &str), Deposits> = Map::new("deposits"); //const is immutable variable, the value cannot be changed or shadowed, we're using if we know that's constant 
+//const is immutable variable, the value cannot be changed or shadowed, we're using if we know that's constant 
+pub const DEPOSITS: Map<(&str, &str), Deposits> = Map::new("deposits"); 
 
 pub const WITHDRAWS: Map<(&str, &str), Withdraws> = Map::new("withdraws");
 
